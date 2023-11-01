37f301d244af537e0f9cd75777d1556928372351
Espen Hellerud <espenhellerud5@gmail.com>
2023-11-01 22:18:27 +0100

    Fix match card ui

--- apps/web/src/components/LiveMatchCard.tsx
+++ apps/web/src/components/LiveMatchCard.tsx
@@ -18,21 +18,23 @@ export default function LiveMatchCard({ match, liveInfo, liveState }: Props) {
       ? ''
       : `: ${match.home_team.name} vs. ${match.away_team.name}`;
   return (
-    <section className="flex items-center gap-8 px-8 border-2 border-primary rounded bg-base-100">
+    <section className="px-8 border-2 border-primary rounded bg-base-100">
       <h3 className="mt-3 text-left font-bold text-lg text-accent">{`${match.config.type}${teamText}`}</h3>
       <div className="divider divider-vertical m-0" />
-      <div className="stat">
-        <div className="stat-title">{match.config.name}</div>
-        <div className="stat-value capitalize">{matchTime}</div>
-        <div className="stat-desc">{matchDate}</div>
-      </div>
-      <div className="stat">
-        <div className="stat-title">{serverName}</div>
-        <div className="stat-value capitalize">{liveState}</div>
-      </div>
-      <div className="stat">
-        <div className="stat-title">Rounds played</div>
-        <div className="stat-value">{match.rounds.length}</div>
+      <div className="flex items-center gap-8">
+        <div className="stat">
+          <div className="stat-title">{match.config.name}</div>
+          <div className="stat-value capitalize">{matchTime}</div>
+          <div className="stat-desc">{matchDate}</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">{serverName}</div>
+          <div className="stat-value capitalize">{liveState}</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Rounds played</div>
+          <div className="stat-value">{match.rounds.length}</div>
+        </div>
       </div>
     </section>
   );
