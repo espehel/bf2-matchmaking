b936cdb9a2fb5499689f1fdbc24a32105ddaf0e9
Espen Hellerud <espenhellerud5@gmail.com>
2023-11-21 19:45:17 +0100

    Fix platform bug

---
+++ apps/platform/src/app.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import Koa from 'koa';
+import logger from 'koa-logger';
+import { bodyParser } from '@koa/bodyparser';
+import { rootRouter } from './routers/root';
+import { error, info } from '@bf2-matchmaking/logging';
+import { loadStartupScripts } from './services/vultr';
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5003;
+loadStartupScripts()
+  .then((startupScripts) => {
+    info('app', `Loaded ${startupScripts.size} startup scripts`);
+  })
+  .catch((e) => {
+    error('app', e);
+  });
+
+new Koa()
+  .use(logger())
+  .use(bodyParser())
+  .use(rootRouter.routes())
+  .use(rootRouter.allowedMethods())
+  .listen(PORT, () => {
+    info('app', `platform listening on port ${PORT}`);
+  });
