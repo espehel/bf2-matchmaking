7fc287f4b3da4adab0f0e01fc2450d543909e9a6
Espen Hellerud <espenhellerud5@gmail.com>
2023-12-11 01:31:37 +0100

    Add more logging

--- apps/platform/src/services/demos.ts
+++ apps/platform/src/services/demos.ts
@@ -1,16 +1,20 @@
 import { api, getText } from '@bf2-matchmaking/utils';
 import { getHrefs } from '@bf2-matchmaking/utils/src/string-utils';
-import { logMessage } from '@bf2-matchmaking/logging';
+import { error, info, logMessage } from '@bf2-matchmaking/logging';
 
 export async function saveDemos(server: string) {
   const demos = await fetchDemos(server);
+  info('saveDemos', `Server ${server}: Found ${demos.length} demos`);
   if (demos.length === 0) {
     return null;
   }
 
-  const { data } = await api.bot().postDemos({ server, demos });
+  const { data, error: err } = await api.bot().postDemos({ server, demos });
   if (data) {
     logMessage(`Server ${server}: Saved ${demos.length} demos`, { ...data });
+  }
+  if (err) {
+    error('saveDemos', err);
   }
 
   return data;
@@ -19,11 +23,9 @@ async function fetchDemos(server: string) {
   const demoPath = `http://${server}/demos`;
   const { data } = await getText(demoPath);
 
-  const demoes = data
+  return data
     ? getHrefs(data)
         .filter((t) => t.startsWith('auto_') && t.endsWith('.bf2demo'))
         .map((name) => `${demoPath}/${name}`)
     : [];
-
-  return demoes;
 }
