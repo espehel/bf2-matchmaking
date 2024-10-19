24315a8705593bf254c1387d4f63928a94c5bd87
Espen Hellerud <espenhellerud5@gmail.com>
2024-10-19 19:22:52 +0200

    fix text util bug

--- packages/utils/src/string-utils.ts
+++ packages/utils/src/string-utils.ts
@@ -1,4 +1,7 @@
 export function fromSnakeToCapitalized(text: string) {
+  if (!text.includes('_')) {
+    return text;
+  }
   return text
     .split('_')
     .map((w) => w[0].toUpperCase().concat(w.slice(1)))
