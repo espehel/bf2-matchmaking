139788f3c7094c9fca6e3a8328c71a4b72999873
Espen Hellerud <espenhellerud5@gmail.com>
2025-02-12 15:48:54 +0100

    add debug logging

--- apps/web/src/app/gather/page.tsx
+++ apps/web/src/app/gather/page.tsx
@@ -6,20 +6,27 @@ import {
   getGatherState,
 } from '@bf2-matchmaking/redis/gather';
 import ServerSection from '@/components/gather/ServerSection';
+import { Suspense } from 'react';
 
 export default async function Page() {
   const events = await api.v2.getGatherEvents(20).then(verify);
+  console.log('events');
   const gatherState = await getGatherState(20);
+  console.log('state');
   const config = await getGatherConfig(20);
+  console.log('config');
   assertObj(config, 'Missing gather config');
   const gatherPlayers = await getGatherQueue(20);
+  console.log('players');
   return (
     <main className="main">
       <h1>{config.name}</h1>
       <p>
         Status: {gatherState.status} ({gatherPlayers.length}/{config.size})
       </p>
-      <ServerSection address={gatherState.address} />
+      <Suspense fallback={null}>
+        <ServerSection address={gatherState.address} />
+      </Suspense>
       <section className="section">
         <h2>Gather events</h2>
         <code className="bg-base-300 p-4 rounded">
